399d88f4e197d7d51f01ea1f366151268c71f63d
Cristian Caloian <cristian.caloian@volvocars.com>
2024-08-20 14:19:59 +0200

    add func for surround with

--- init.lua
+++ init.lua
@@ -841,4 +841,29 @@ cmp.setup {
 -- vim: ts=2 sts=2 sw=2 et
 vim.wo.relativenumber = true
 --
+-- function Surround(w_or_W)
+--     local open_char = vim.fn.input("Surround with: ")
+--     local closed_char = nil
+--     if open_char == "(" then closed_char = ")" end
+--     if open_char == "[" then closed_char = "]" end
+--     if open_char == "{" then closed_char = "}" end
+--     if open_char == "<" then closed_char = ">" end
+--     if open_char == "'" then closed_char = "'" end
+--     if open_char == '"' then closed_char = '"' end
+--     if open_char == "`" then closed_char = "`" end
+--     if open_char == "/" then closed_char = "/" end
+--     if open_char == "|" then closed_char = "|" end
+--
+--     if w_or_W == "w" then
+--         vim.cmd("normal! ciw" .. open_char)
+--     elseif w_or_W == "W" then
+--         vim.cmd([[normal! ciW]] .. open_char)
+--     end
+--     vim.cmd("normal! p")
+--     vim.cmd("normal! a" .. closed_char)
+--     vim.cmd("normal! a")
+-- end
+--
+-- vim.api.nvim_set_keymap("n", "<Space>xw", ":lua Surround('w')<CR>", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<Space>xW", ":lua Surround('W')<CR>", { noremap = true, silent = true })
 
