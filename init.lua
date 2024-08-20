edc90330b1ed976ffae147a2f92b0da8cd02d048
Cristian Caloian <cristian.caloian@volvocars.com>
2024-08-20 14:15:07 +0200

    add nvim-surround plugin

--- init.lua
+++ init.lua
@@ -318,6 +318,19 @@ require('lazy').setup({
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
   },
+
+  -- nvim surround 
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  },
+
 }, {})
 
 -- Telescop file browser setup
