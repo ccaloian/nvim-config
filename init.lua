8c647abeae2c9ae73b3225edca129abed33546c9
Cristian Caloian <cristian.caloian@volvocars.com>
2024-08-20 14:16:37 +0200

    delete buffer shortcut telescope

--- init.lua
+++ init.lua
@@ -545,8 +545,11 @@ require('telescope').setup {
     mappings = {
       i = {
         ['<C-u>'] = false,
-        ['<C-d>'] = false,
+        ['<C-d>'] = require('telescope.actions').delete_buffer,
       },
+      n = {
+        ['<C-d>'] = require('telescope.actions').delete_buffer,
+      }
     },
   },
 }
